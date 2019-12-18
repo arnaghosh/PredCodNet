23b1f1fbd069fcf793d490328baa350dbf1dd134
Arna Ghosh <arnatubaikgp@gmail.com>
2019-12-18 03:12:16 -0500

    adding codes and logs for ELBO-updated CVAE

---
+++ CVAE_ELBO/run_train_latent_tfo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+module load python/3.7
+
+module load cuda/10.1 cuda-10.1/cudnn/7.6
+
+source $HOME/torchenv/bin/activate
+
+python train_latent_tfo.py > $SLURM_TMPDIR/log_CVAE_Ztfo1++_MNIST.txt
+
+cp $SLURM_TMPDIR/log_CVAE_Ztfo1++_MNIST.txt .
+cp $SLURM_TMPDIR/CVAE_Ztfo1_MLP_model++.pt .
+cp $SLURM_TMPDIR/*.png .
